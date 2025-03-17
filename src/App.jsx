84c992bba55f361a7235dae74a24e6de58688414
edupoly <info.edupoly@gmail.com>
2025-03-17 09:05:12 +0530

    graphql

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useQuery, gql } from '@apollo/client';
+const GET_BOOKS = gql`
+  query MyBooks {
+    kithab {
+      id
+      title
+      author
+    }
+  }
+`;
 function App() {
+  const { loading, error, data } = useQuery(GET_BOOKS);
   return (
     <div>
       <h1 className="bg-success">Hello Soundarya</h1>
+      {loading && <b>Loading...</b>}
+      {!loading && <i>{JSON.stringify(data)}</i>}
     </div>
   );
 }
