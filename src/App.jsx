cb223e6b9881fdcb9a4c2b8de1933e7958801b8e
edupoly <info.edupoly@gmail.com>
2025-03-19 09:33:40 +0530

    sdfsdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useQuery, gql } from '@apollo/client';
+const GET_BOOKS = gql`
+                      query GetAllBooks {
+                        getAllBooks {
+                          author
+                          title
+                        }
+                      }
+`;
 function App() {
+  const { loading, error, data }= useQuery(GET_BOOKS)
   return (
     <div>
-      <h1 className="bg-success">Hello Soundarya</h1>
+      <h1 className="bg-success">Hello GraphQL</h1>
+      {
+        loading && <b>Loading.....</b>
+      }
+      {
+        !loading && data?.getAllBooks.map((book)=>{
+          return <li>{book.title}</li>
+        })
+      }
     </div>
   );
 }
