c0de485ba45ff1fcce494dfe25ed9f8c19786dc7
edupoly <info.edupoly@gmail.com>
2025-03-20 12:46:05 +0530

    sdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql, useMutation } from '@apollo/client';
+import { useState } from 'react';
 
+const GET_BOOKS = gql`
+  query GetAllBooks {
+    getAllBooks {
+      author
+      title
+      id
+    }
+  }
+`;
+const CREATE_BOOK=gql`
+    mutation CreateBook($title: String!, $author: String) {
+      createBook(title: $title, author: $author) {
+        author
+        id
+        title
+      }
+    }
+
+`
 function App() {
+  const { loading, error, data } = useQuery(GET_BOOKS);
+  const [addBookFn]=useMutation(CREATE_BOOK)
+  var [newBook,setNewBook] = useState({
+    author:"",
+    title:"",
+
+  })
+  function addBook(){
+    addBookFn({
+      variables:newBook
+    }).then((res)=>{
+      console.log(res);
+    })
+  }
   return (
     <div>
       <h1 className="bg-success">Hello Soundarya</h1>
+      <input type="text" onChange={(e)=>{setNewBook({...newBook,author:e.target.value})}} placeholder='Enter Author'/>
+      <br />
+      <input type="text" onChange={(e)=>{setNewBook({...newBook,title:e.target.value})}} placeholder='Enter Title'/>
+      <br />
+      <button onClick={addBook}>Add Book</button>
+      <ul>
+        {
+          data?.getAllBooks.map((book)=>{
+            return <li>{book.title}</li>
+          })
+        }
+      </ul>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/',
+    cache: new InMemoryCache(),
+  });
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
